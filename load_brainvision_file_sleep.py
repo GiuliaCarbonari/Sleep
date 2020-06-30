becb4dc7eff366c15958d6e3677207a935d4c520
GiuliaCarbonari <65787219+GiuliaCarbonari@users.noreply.github.com>
2020-06-30 17:07:22 -0300

    Update

--- load_brainvision_file_sleep.py
+++ load_brainvision_file_sleep.py
@@ -8,6 +8,7 @@ https://www.dropbox.com/s/t2bo9ufvc3f8mbj/sleep_brainvision.zip?dl=1
 
 .. image:: ../../_static/examples/ex_LoadBrainVision.png
 """
+import mne
 import os
 from visbrain.gui import Sleep
 from visbrain.io import download_file, path_to_visbrain_data
@@ -15,22 +16,28 @@ from visbrain.io import download_file, path_to_visbrain_data
 ###############################################################################
 #                               LOAD YOUR FILE
 ###############################################################################
+# Directory:
+dfile='C:/Users/Nicola/Documents/eeg/VISBRAIN/ExpS35.vhdr'
+hfile = None            #Al cargarlo como vacio el programa me lo va a pedir pero puedo no ponerlo
+cfile = None
+
+raw = mne.io.read_raw_brainvision(vhdr_fname=dfile, preload=True)
+
+# Extract data, sampling frequency and channels names
+data = raw.get_data() * 1e6      # Convert Volts to uV
+sf = raw.info['sfreq']           # sampling frequency
+channels = raw.ch_names
+
+# Now, pass all the arguments to the Sleep module :
+Sleep(data=data, sf=sf, channels=channels).show() 
+
+
+
+# Alternatively, these steps can be done automatically by using the 'use_mne'
+# input argument of sleep
+#Sleep(data=dfile, hypno=hfile, use_mne=True).show()  
+
 
 # Run the interface (requires loading of a data file):
-Sleep().show() # Load file from the computer
-
-#SIN RESOLVER:No pude cargar mis archivos desde mi Pc de la forma de abajo
-
-"""
-# Download dataset :                                                         #From mi pc
-download_file("sleep_brainvision.zip", unzip=True, astype='example_data')
-target_path = path_to_visbrain_data(folder='example_data')                   #target_path ??
-
-dfile = os.path.join(target_path, 'sub-02.vhdr')                             #dfile ='C:/Users/Nicola/Documents/eeg/VISBRAIN/datos_ceci/ExpS35.vhdr'
-hfile = os.path.join(target_path, 'sub-02.hyp')                              #hfile = None
-cfile = os.path.join(target_path, 'sub-02_config.txt')                       #cfile = None
-
-# Open the GUI :
-Sleep(data=dfile, hypno=hfile, config_file=cfile).show()                     #Sleep(data=dfile, hypno=hfile, config_file=cfile).show()
-#                                                                            #Sleep(data=dfile).show()
+#Sleep().show() # Load file from the computer                                                                      #Sleep(data=dfile).show()
 """
